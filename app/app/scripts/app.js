ef3feb3253469ba580fd49baf2e840b7fc37c8fc
prasad8mhatre <prasad.mhatre@coditas.com>
2017-04-25 09:20:41 +0530

    Added Dnd list

--- app/app/scripts/app.js
+++ app/app/scripts/app.js
@@ -9,89 +9,95 @@
  * Main module of the application.
  */
 var app = angular.module('kanbanApp', [
-    'ui.router',
-    'ncy-angular-breadcrumb',
-    'angular-loading-bar'
+  'ui.router',
+  'ncy-angular-breadcrumb',
+  'angular-loading-bar',
+  'dndLists'
 ]);
 
 
 
 app.config(['$httpProvider', function($httpProvider) {
-    $httpProvider.defaults.withCredentials = true;
-    $httpProvider.interceptors.push(function($q, $location) {
-        return {
-            'responseError': function(rejection) {
-                if (rejection.status === 401) {
-                    $location.url("/");
-                }
-                return $q.reject(rejection);
-            }
-        };
-    });
+  $httpProvider.defaults.withCredentials = true;
+  $httpProvider.interceptors.push(function($q, $location) {
+    return {
+      'responseError': function(rejection) {
+        if (rejection.status === 401) {
+          $location.url("/");
+        }
+        return $q.reject(rejection);
+      }
+    };
+  });
 }]);
 
 app.config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider.state('app', {
-        url: '/app',
-        templateUrl: 'views/main.html'
-    });
+  $stateProvider.state('app', {
+    url: '/app',
+    templateUrl: 'views/main.html'
+  });
 
-    $stateProvider.state('app.home', {
-        url: '/home',
-        templateUrl: 'views/home/home.html',
-        data: {
-            title: 'Home'
-        },
-        ncyBreadcrumb: {
-          label: 'Home'
-        }
-    }).state('app.home.teams', {
-        url: '/teams',
-        templateUrl: 'views/home/teams.html',
-        controller: 'TeamsCtrl',
-        data: {
-            title: 'Teams'
-        },
-        ncyBreadcrumb: {
-          label: 'Team'
-        }
-    });
+  $stateProvider.state('app.home', {
+    url: '/home',
+    templateUrl: 'views/home/home.html',
+    data: {
+      title: 'Home'
+    },
+    ncyBreadcrumb: {
+      label: 'Home'
+    }
+  }).state('app.home.teams', {
+    url: '/teams',
+    templateUrl: 'views/home/teams.html',
+    controller: 'TeamsCtrl',
+    data: {
+      title: 'Teams'
+    },
+    ncyBreadcrumb: {
+      label: 'Team'
+    }
+  });
 
-    $stateProvider.state('app.home.teams.boards', {
-        url: '/boards',
-        templateUrl: 'views/home/boards.html',
-        controller: 'BoardsCtrl',
-        data: {
-            title: 'Boards'
-        },
-        ncyBreadcrumb: {
-          label: 'Boards'
-        }
-    }).state('app.home.teams.boards.board', {
-          url: '/board',
-          templateUrl: 'views/home/board.html',
-          controller: 'BoardCtrl',
-          data: {
-              title: 'Board'
-          },
-          ncyBreadcrumb: {
-            label: 'Board'
-          }
-      });;
+  $stateProvider.state('app.home.teams.boards', {
+    url: '/boards',
+    templateUrl: 'views/home/boards.html',
+    controller: 'BoardsCtrl',
+    data: {
+      title: 'Boards'
+    },
+    ncyBreadcrumb: {
+      label: 'Boards'
+    }
+  }).state('app.home.teams.boards.boardsDetails', {
+    url: '/board/:boardId',
+    templateUrl: 'views/home/board.html',
+    controller: 'BoardCtrl',
+    data: {
+      title: 'Board'
+    },
+    ncyBreadcrumb: {
+      label: '{{boardId}}'
+    },
+   resolve:{
+      boardId: ['$stateParams', function($stateParams){
+          return $stateParams.boardId;
+      }]
+   }
+  });
 
 
-    $stateProvider.state('app.dash', {
-        url: '/dash',
-        templateUrl: 'views/home/dash.html',
-        data: {
-            title: 'Dash'
-        },
-        ncyBreadcrumb: {
-          label: 'Dash'
-        }
-    });
+  $stateProvider.state('app.dash', {
+    url: '/dash',
+    templateUrl: 'views/home/dash.html',
+    data: {
+      title: 'Dash'
+    },
+    ncyBreadcrumb: {
+      label: 'Dash'
+    }
+  });
 
-    $urlRouterProvider.otherwise('/app/home');
+  $urlRouterProvider.otherwise('/app/home');
 });
 
 app.constant('serverUrl', 'localhost:3000');
--- app/app/scripts/controllers/home/boardCtrl.js
+++ app/app/scripts/controllers/home/boardCtrl.js
@@ -1,7 +1,62 @@
-app.controller('BoardCtrl', [ '$scope', '$state', function($scope, $state) {
+app.controller('BoardCtrl', ['$scope', '$state', 'boardId', function($scope, $state, boardId) {
 
-   console.log("In BoardCtrl ");
-   $scope.Hello = "Hello";
-   $scope.lists = ["1","2","3"];
+  console.log("In BoardCtrl ");
+  $scope.Hello = "Hello";
+  $scope.lists = ["1", "2", "3"];
+  $scope.boardId = boardId;
+
+  $scope.models = {
+    selected: null,
+    lists: [{
+      name: "Category 1",
+      items: [{
+        name: "Item 1.1",
+        description :"hee"
+      }, {
+        name: "Item 1.2",
+        description :"hee"
+      }, {
+        name: "Item 1.3",
+        description :"hee"
+      }]
+    }, {
+      name: "Category 2",
+      items: [{
+        name: "Item 2.1",
+        description :"hee"
+      }, {
+        name: "Item 2.2",
+        description :"hee"
+      }, {
+        name: "Item 2.3",
+        description :"hee"
+      }]
+    }, {
+      name: "Category 3",
+      items: [{
+        name: "Item 3.1",
+        description :"hee"
+      }, {
+        name: "Item 3.2",
+        description :"hee"
+      }, {
+        name: "Item 3.3",
+        description :"hee"
+      }]
+    },
+    {
+      name: "Category 4",
+      items: [{
+        name: "Item 3.1",
+        description :"hee"
+      }, {
+        name: "Item 3.2",
+        description :"hee"
+      }, {
+        name: "Item 3.3",
+        description :"hee"
+      }]
+    }]
+  };
 
 }]);
